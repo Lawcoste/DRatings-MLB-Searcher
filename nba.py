e7bf64953f925f1b9317ea14f5f707c4af914f08
Lawcoste <73073205+Lawcoste@users.noreply.github.com>
2024-05-06 20:12:31 +0100

    Update nba.py

--- nba.py
+++ nba.py
@@ -1,3 +1,9 @@
+# NBA Version
+
+import requests
+from bs4 import BeautifulSoup
+import statistics
+
 import requests
 from bs4 import BeautifulSoup
 import statistics
@@ -9,7 +15,7 @@ def american_to_decimal(american_odds):
         return round((100 / abs(american_odds)) + 1, 2)
     else:
         return 1.00  # If American odds are 0, return decimal odds of 1.00
-
+        
 def get_predictions(url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -30,30 +36,47 @@ def get_predictions(url):
                     team_names = []  # List to store team names in a match
                     green_values = []  # List to store values associated with the teams
                     decimal_odds = []
-                    
-                    # Runs through the row to find the team names and odds
+
+
+
                     for cell in cells:
-                        if cell.get("class") == ["ta--left", "tf--body"]:
+                        if cell.get("class") == ["table-division"]:
                             # Within each of these td elements, find the span elements
-                            span_elements = cell.find_all("span", class_="table-cell--mw d--ib lh--12")
-                            for span in span_elements:
-                                team_names.append(span.text.strip())  # Add team name to the list
+                            green_value = cell.find("span", class_="tc--green")
+                            if green_value:
+                                green_value = green_value.text.strip().replace('%', '')  # Remove percentage sign
+                                green_value = float(green_value)  # Convert to float
+                                if green_value >= 70: # Only find team names and odds if dratings confidence >= 70
+                                    
+                                    # Runs through the row to find the 2 team names
+                                    for cell in cells:
+                                        if cell.get("class") == ["ta--left", "tf--body"]:
+                                            # Within each of these td elements, find the span elements
+                                            span_elements = cell.find_all("span", class_="table-cell--mw d--ib lh--12")
+                                            for span in span_elements:
+                                                team_names.append(span.text.strip())  # Add team name to the list
+                
+                                    # Find the odds within the current row
+                                    if not green_values:
+                                        vegas_sportsbook_div = row.find('div', class_='vegas-sportsbook')
+                                        if vegas_sportsbook_div:
+                                            odd = vegas_sportsbook_div.text.strip()
+                                            dash_count = odd.count("-")
+                                            
+                                            # If both are somehow favourites, just take the first odds value since they're usually around the same
+                                            if dash_count>=2:
+                                                odd_parts = odd.split('-')
+                                                odd = odd_parts[0]
+                                            elif odd[0] == '-':
+                                                odd = odd.split('+')[0]
+                                            else:
+                                                odd = odd.split('-')[1]
+                                                odd = "-" + odd
+                                                
+                                            odd = int(odd)
+                                            decimal_odd = american_to_decimal(odd)
+                                            decimal_odds.append(decimal_odd)
                                 
-                    # Find the odds within the current row
-                    if not green_values:
-                        vegas_sportsbook_div = row.find('div', class_='vegas-sportsbook')
-                        if vegas_sportsbook_div:
-                            odd = vegas_sportsbook_div.text.strip()
-                            if odd[0] == '-':
-                                odd = odd.split('+')[0]
-                            else:
-                                odd = odd.split('-')[1]
-                                odd = "-" + odd
-                            odd = int(odd)
-                            decimal_odd = american_to_decimal(odd)
-                            decimal_odds.append(decimal_odd)
-                            
-                            
                     # Find the td elements with class "table-division"
                     for cell in cells:
                         if cell.get("class") == ["table-division"]:
@@ -64,7 +87,7 @@ def get_predictions(url):
                                 green_value = float(green_value)  # Convert to float
                                 if green_value >= 70:
                                     green_values.append(green_value)
-                    
+
                     # Associate team names with values if at least one team has a confidence value above 70%
                     if len(team_names) == 2 and len(green_values) > 0:
                         teams_data.append({"team_name_1": team_names[0], "team_name_2": team_names[1], "green_value": green_values[0], "odds": decimal_odds[0]})
@@ -74,14 +97,13 @@ def get_predictions(url):
         
         return teams_data
     else:
-        print(response.status_code)
         print("Failed to fetch predictions")
 
 if __name__ == "__main__":
     sport_url = "https://www.dratings.com/predictor/nba-basketball-predictions/"
     # Define the URL for the first page
     page_number = 1
-    max_pages = 5  # Maximum number of pages to fetch predictions from
+    max_pages = 10  # Maximum number of pages to fetch predictions from
     total_odds = []
     
     while page_number <= max_pages:
