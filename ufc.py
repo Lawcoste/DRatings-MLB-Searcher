30eb87aef429810272e578f67b3b102da4963e34
Lawcoste <73073205+Lawcoste@users.noreply.github.com>
2024-05-20 22:14:03 +0100

    Update ufc.py, main changes complete
    Fully functional

--- ufc.py
+++ ufc.py
@@ -33,11 +33,11 @@ def get_predictions(url):
                     green_values = []  # List to store values associated with the teams
                     decimal_odds = []
                     fighter_win = []
+                    win_list = []
                     fighter_position = -1
                     win_or_lose = ""
                     
                     
-
                     for cell in cells:
                         if cell.get("class") == ["table-division"]:  # Filter for confidence
                             # Within each of these td elements, find the span elements
@@ -76,76 +76,37 @@ def get_predictions(url):
                                         elif cell.text == "LossWin" and fighter_position == 1:
                                           win_or_lose = cell.text.strip("Loss")
                                           
-                                      #team_names.append(spany_elements[fighter_position].text)
-                                      print(spany_elements[fighter_position].text, "@", green_value, ":", win_or_lose)
+                                          
+                                      
+                                      vegas_sportsbook_div = row.find('div', class_='vegas-sportsbook')
+                                      if vegas_sportsbook_div:
+                                          odd = vegas_sportsbook_div.text.strip()
+                                          dash_count = odd.count("-")
+                                          
+                                          # If both are somehow favourites, just take the first odds value since they're usually around the same
+                                          if dash_count>=2:
+                                              odd_parts = odd.split('-')
+                                              odd = odd_parts[0]
+                                          elif not odd:
+                                              odd = 0
+                                          elif odd[0] == '-':
+                                              odd = odd.split('+')[0]
+                                          else:
+                                              odd = odd.split('-')[1]
+                                              odd = "-" + odd
+                                              
+                                          odd = int(odd)
+                                          decimal_odd = american_to_decimal(odd)
+                                          decimal_odds.append(decimal_odd)
+                                          
+                                          
+                                      # Append to all the arrays
+                                      team_names.append(spany_elements[fighter_position].text)
+                                      green_values.append(green_value)
+                                      win_list.append(win_or_lose)
+                                      
+                                      teams_data.append({"fighter_name": spany_elements[fighter_position].text, "green_value": green_value, "odds": decimal_odd, "win_or_lose": win_or_lose})
                                         
-                                
-                    
-
-                    for cell in cells:
-                        if cell.get("class") == ["table-division"]: # Filter for confidence
-                            # Within each of these td elements, find the span elements
-                            green_value = cell.find("span", class_="tc--green")
-                            if green_value:
-                                green_value = green_value.text.strip().replace('%', '')  # Remove percentage sign
-                                green_value = float(green_value)  # Convert to float
-                                if green_value >= 70: # Only find team names and odds if dratings confidence >= 70
-                                    
-                                    # Runs through the row to find the 2 team names
-                                    for cell in cells:
-                                        fighter_position = 1
-                                        if cell.get("class") == ["ta--left", "tf--body"]:
-                                            # Within each of these td elements, find the span elements
-                                            span_elements = cell.find_all("span", class_="table-cell--mw d--ib lh--12")
-                                            for span in span_elements:
-                                                if fighter_position == 1:
-                                                  #print(span.text.strip())
-                                                  fighter_position = 2
-                                                else:
-                                                  #print(span.text.strip())
-                                                  fighter_position = 1
-                                                team_names.append(span.text.strip())  # Add team name to the list
-                                                
-                
-                                    # Find the odds within the current row
-                                    if not green_values:
-                                        vegas_sportsbook_div = row.find('div', class_='vegas-sportsbook')
-                                        if vegas_sportsbook_div:
-                                            odd = vegas_sportsbook_div.text.strip()
-                                            dash_count = odd.count("-")
-                                            
-                                            # If both are somehow favourites, just take the first odds value since they're usually around the same
-                                            if dash_count>=2:
-                                                odd_parts = odd.split('-')
-                                                odd = odd_parts[0]
-                                            elif not odd:
-                                                odd = 0
-                                            elif odd[0] == '-':
-                                                odd = odd.split('+')[0]
-                                            else:
-                                                odd = odd.split('-')[1]
-                                                odd = "-" + odd
-                                                
-                                            odd = int(odd)
-                                            decimal_odd = american_to_decimal(odd)
-                                            decimal_odds.append(decimal_odd)
-                                            
-      
-                                
-                    # Find the td elements with class "table-division"
-                    for cell in cells:
-                        if cell.get("class") == ["table-division"]:
-                            # Within each of these td elements, find the span elements
-                            green_value = cell.find("span", class_="tc--green")
-                            if green_value:
-                                green_value = green_value.text.strip().replace('%', '')  # Remove percentage sign
-                                green_value = float(green_value)  # Convert to float
-                                if green_value >= 70:
-                                    green_values.append(green_value)
-
-                    # Associate team names with values if at least one team has a confidence value above 70%
-                    if len(team_names) == 2 and len(green_values) > 0:
-                        teams_data.append({"team_name_1": team_names[0], "team_name_2": team_names[1], "green_value": green_values[0], "odds": decimal_odds[0]})
                                 
         else:
             print("Parent div with ID 'scroll-completed' not found.")
@@ -154,20 +115,31 @@ def get_predictions(url):
     else:
         print("Failed to fetch predictions")
 
+
+# 30, 40, 50, 100 pages -> 80%
+# 10, 20 pages -> 76%
 if __name__ == "__main__":
     sport_url = "https://www.dratings.com/predictor/ufc-mma-predictions/"
     # Define the URL for the first page
     page_number = 1
-    max_pages = 1  # Maximum number of pages to fetch predictions from
+    max_pages = 13  # Maximum number of pages to fetch predictions from
     total_odds = []
+    total_wins_counter = 0
+    total_bets_counter = 0
     
     while page_number <= max_pages:
         url = "{}completed/{}#scroll-completed".format(sport_url, page_number)
         predictions = get_predictions(url)
         if predictions:
-            print("\n Predictions with confidence score > 70% for page {}:".format(page_number))
+            print("\nPredictions with confidence score > 70% for page {}:".format(page_number))
             for prediction in predictions:
-                print("- {} vs {}: {}% ({})".format(prediction['team_name_1'], prediction['team_name_2'], prediction['green_value'], prediction["odds"]))
+                if prediction['win_or_lose'] == "Win":
+                  total_wins_counter+=1
+                  total_bets_counter+=1
+                else:
+                  total_bets_counter+=1
+                  
+                print("{}, {}%, {}, {}".format(prediction['fighter_name'], prediction['green_value'], prediction['win_or_lose'], prediction["odds"]))
                 total_odds.append(prediction['odds'])
             page_number += 1
         else:
@@ -175,4 +147,6 @@ if __name__ == "__main__":
             page_number += 1
 
     mean_odds = statistics.mean(total_odds)
+    win_rate = total_wins_counter/total_bets_counter
+    print("\nWin rate", total_wins_counter, "/", total_bets_counter, "=", win_rate)
     print("Average Odds: ", mean_odds)
